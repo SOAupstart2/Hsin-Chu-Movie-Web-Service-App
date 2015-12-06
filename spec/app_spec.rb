7f5b158bdb57a670ad9b478bfd8cd6146d978237
pengyuchen <pengyu@nlplab.cc>
2015-12-06 20:07:28 +0800

    pageobject

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -26,6 +26,7 @@ describe 'Kandianying' do
     it 'finds main components' do
       visit HistoryPage do |page|
         page.id_fortest1.must_equal 'Movie Timeline'
+        page.cinema_table?.must_equal true
         page.location_button?.must_equal true
         page.language_button?.must_equal true
       end
